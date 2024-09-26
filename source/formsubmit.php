06f3ee2d106ba5c7910f6dee9aab6e59c37efc45
KynanPR <kynanpr@gmail.com>
2024-09-26 14:31:55 +0100

    Pointed form to new email

--- source/formsubmit.php
+++ source/formsubmit.php
@@ -5,7 +5,7 @@ $phone = $_POST['phone'];
 $gender = $_POST['gender'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Phone: $phone \n Gender: $gender \n Message: $message";
-$recipient = "kynanpr@gmail.com";
+$recipient = "contact@blackbirdcounselling.co.uk";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
