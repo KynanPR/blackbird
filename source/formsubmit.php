52129bc3223ae83124788d305135685f43552894
KynanPR <kynanpr@gmail.com>
2024-09-28 18:02:36 +0100

    Contact form

--- source/formsubmit.php
+++ source/formsubmit.php
@@ -1,13 +1,13 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$gender = $_POST['gender'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Phone: $phone \n Gender: $gender \n Message: $message";
-$recipient = "contact@blackbirdcounselling.co.uk";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $formcontent="From: $name \n Message: $message";
+    $recipient = "contact@blackbirdcounselling.co.uk";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
 ?>
--- source/scripts.js
+++ source/scripts.js
@@ -1,64 +1,71 @@
 function loadHeader() {
-	// Manually perform scroll on nav link click to have it be smooth and account for the header
-	const header = document.querySelector("header");
-	const navLinks = header.querySelectorAll("nav a");
+  // Manually perform scroll on nav link click to have it be smooth and account for the header
+  const header = document.querySelector("header");
+  const navLinks = header.querySelectorAll("nav a");
 
-	navLinks.forEach((link) => {
-		link.addEventListener("click", handleNavigationClick);
-	});
+  navLinks.forEach((link) => {
+    link.addEventListener("click", handleNavigationClick);
+  });
 
-	function handleNavigationClick(event) {
-		event.preventDefault();
-		const targetSection = event.target.getAttribute("href");
-		scrollToSection(targetSection);
-	}
+  function handleNavigationClick(event) {
+    event.preventDefault();
+    const targetSection = event.target.getAttribute("href");
+    scrollToSection(targetSection);
+  }
 
-	function scrollToSection(targetSection) {
-		const navbarHeight = header.offsetHeight;
-		const targetElement = document.querySelector(targetSection);
-		const targetPosition = targetElement.offsetTop;
+  function scrollToSection(targetSection) {
+    const navbarHeight = header.offsetHeight;
+    const targetElement = document.querySelector(targetSection);
+    const targetPosition = targetElement.offsetTop;
 
-		window.scrollTo({
-			top: targetPosition - (navbarHeight + 50),
-			behavior: "smooth",
-		});
-	}
+    window.scrollTo({
+      top: targetPosition - (navbarHeight + 50),
+      behavior: "smooth",
+    });
+  }
 }
 
 function loadGallery() {
-	const gallerySection = document.querySelector(".gallery-section");
-	const galleryCollapsible = gallerySection.querySelector(".gallery__collapsible");
-	const galleryButton = gallerySection.querySelector(".gallery__button");
+  const gallerySection = document.querySelector(".gallery-section");
+  const galleryCollapsible = gallerySection.querySelector(
+    ".gallery__collapsible"
+  );
+  const galleryButton = gallerySection.querySelector(".gallery__button");
 
-	galleryButton.addEventListener("click", toggleState);
+  galleryButton.addEventListener("click", toggleState);
 
-	// Defaults to expanded in case JS doesn't load, but as soon as JS loads, colapse it down
-	// Manually add rather than using toggleState() to avoid usual scrolling behaviour
-	galleryButton.classList.add("gallery__button--collapsed");
-	galleryCollapsible.classList.add("gallery__collapsible--collapsed");
+  // Defaults to expanded in case JS doesn't load, but as soon as JS loads, colapse it down
+  // Manually add rather than using toggleState() to avoid usual scrolling behaviour
+  galleryButton.classList.add("gallery__button--collapsed");
+  galleryCollapsible.classList.add("gallery__collapsible--collapsed");
 
-	function toggleState() {
-		// const startState = currentState;
-		if (galleryCollapsible.classList.toggle("gallery__collapsible--collapsed")) {
-			galleryCollapsible.scrollIntoView({ behavior: "smooth", block: "nearest" });
-		}
-		galleryButton.classList.toggle("gallery__button--collapsed");
-		console.log("toggled state");
-	}
+  function toggleState() {
+    // const startState = currentState;
+    if (
+      galleryCollapsible.classList.toggle("gallery__collapsible--collapsed")
+    ) {
+      galleryCollapsible.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+      });
+    }
+    galleryButton.classList.toggle("gallery__button--collapsed");
+    console.log("toggled state");
+  }
 }
 
 function SubmitFormData() {
-	var name = $("#name").val();
-	var email = $("#email").val();
-	var phone = $("#phone").val();
-	var gender = $("input[type=radio]:checked").val();
-	var message = $("#message").val();
-	$.post(
-		"./source/formsubmit.php",
-		{ name: name, email: email, phone: phone, gender: gender, message: message },
-		function (data) {
-			$("#results").html(data);
-			$("#myForm")[0].reset();
-		}
-	);
+  const name = $("#name").val();
+  const email = $("#email").val();
+  const message = $("#message").val();
+  const honeypot = $("#honeypot").val();
+
+  // Only submit the form if the hidden field is empty to catch out bots
+  if (!honeypot) {
+    $.post(
+      "./source/formsubmit.php",
+      { name: name, email: email, message: message },
+      () => $("#contact-form")[0].reset()
+    );
+  }
 }
